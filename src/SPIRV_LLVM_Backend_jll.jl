2b05872714881f1590551e8599576e8916024f5e
jlbuild <juliabuildbot@gmail.com>
2026-06-03 21:23:05 +0000

    SPIRV_LLVM_Backend_jll build 22.1.7+0

--- src/SPIRV_LLVM_Backend_jll.jl
+++ src/SPIRV_LLVM_Backend_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("SPIRV_LLVM_Backend")
-JLLWrappers.@generate_main_file("SPIRV_LLVM_Backend", UUID("4376b9bf-cff8-51b6-bb48-39421dff0d0c"))
+JLLWrappers.@generate_main_file("SPIRV_LLVM_Backend", Base.UUID("4376b9bf-cff8-51b6-bb48-39421dff0d0c"))
 end  # module SPIRV_LLVM_Backend_jll
